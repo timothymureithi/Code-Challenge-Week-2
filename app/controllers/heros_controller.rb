ea147a81285fe28dd24d108d281fa3091b5eb4e2
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-02 15:18:07 +0300

    hero private method added/render:Error

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,14 +1,20 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
     def index
-    @heroes = Hero.all
-    render json: @heroes
+    heros = Hero.all
+    render json: heros
     end
 
     def show
-        render json: @hero, serializer: HeroSerializer
+        hero = Hero.find(params[:id])
+        render json: hero, serializer: HeroSerializer
     end
-    
+
     private
+
+    def render_not_found
+        render json: { error: "Hero not found"}, status: :not_found
+    end
    
 end
