0375801cf7a56af070c36370b7ea90e1bf62a01e
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-02 19:33:19 +0300

    POST/hero_powers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,3 +1,11 @@
 class HeroPowersController < ApplicationController
 
+    def create
+        hero_power = HeroPower.create!(heropower_param)
+        render json: hero_power.hero, status: :created
+    end
+
+    private
+
+
 end
