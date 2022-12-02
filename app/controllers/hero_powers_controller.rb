8821e316bcef21a0252e6f053f38b2e4e3ff2c60
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-02 13:33:06 +0300

    added routes

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,9 @@
 class HeroPowersController < ApplicationController
+
+    def index
+        render json: HeroPower.all, status: :ok
+    end
+
+    def create
+    end
 end
--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,16 @@
 class HerosController < ApplicationController
+
+    def index
+        heros = Hero.all 
+        render json: heros 
+    end
+
+    def show
+        hero = Hero.find_by(id: params[:id])
+        if hero
+            render json: hero, include: :powers 
+        else
+            render json: {error: "Hero not found"}, status: :not_found
+        end
+    end
 end
