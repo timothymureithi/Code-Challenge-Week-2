6a71fd520d322838727cd906bb62963934d940b9
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-02 20:34:40 +0300

    update hero_power route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :hero_powers, only: [:index, :create]
+  resources :hero_powers, only: [:create]
   resources :powers, only: [:index, :show, :update]
   resources :heros, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
