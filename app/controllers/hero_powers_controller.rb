f50997a004139cc80b2d9a86cf065924906a8f10
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-02 19:36:52 +0300

    private methods: HeroPower

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,5 +1,5 @@
 class HeroPowersController < ApplicationController
-
+    rescue_from ActiveRecord::RecordInvalid, with: :validation_error
     def create
         hero_power = HeroPower.create!(heropower_param)
         render json: hero_power.hero, status: :created
@@ -7,5 +7,12 @@ class HeroPowersController < ApplicationController
 
     private
 
+    def validation_error
+        render json: { error: ["Validation errors"]}, status: 422
+    end
+
+    def heropower_param
+        params.permit(:strength, :power, :hero_id)
+    end
 
 end
