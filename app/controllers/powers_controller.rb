ae0f1669d9137c637687ea1716c34fbab76afcad
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-02 18:04:57 +0300

    GET  power/:id

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,7 +1,14 @@
 class PowersController < ApplicationController
+    
     def index
         powers = Power.all
         render json: powers
     end
 
+    def show
+        power = find_power
+        render json: power
+    end
+
+    
 end
